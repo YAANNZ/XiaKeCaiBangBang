ccfea29e9933f99635a3dba63bfbc3537ab7ac64
朱亚男 <zhuyanan@hemeicloud.com>
2015-10-10 17:56:03 +0800

    添加控制器

---
+++ XiaKeCBB/XKCMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  XKCMainViewController.h
+//  XiaKeCBB
+//
+//  Created by doubin on 15/10/10.
+//  Copyright © 2015年 xiakecbb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XKCMainViewController : UIViewController
+
+@end
---
+++ XiaKeCBB/XKCMainViewController.m
@@ -0,0 +1,55 @@
+//
+//  XKCMainViewController.m
+//  XiaKeCBB
+//
+//  Created by doubin on 15/10/10.
+//  Copyright © 2015年 xiakecbb. All rights reserved.
+//
+
+#import "XKCMainViewController.h"
+#import "UIView+Extension.h"
+
+@interface XKCMainViewController ()
+
+@end
+
+@implementation XKCMainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UILabel *lable = [[UILabel alloc] init];
+    lable.width = 200;
+    lable.height = 100;
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
